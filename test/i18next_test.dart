cd5c35ae3d3e9cb8e0c0364f3f3afdef4bf9811a
William Cho <williamhjcho@gmail.com>
2019-10-22 00:59:10 -0300

    Improves and adds more test cases for i18next

--- test/i18next_test.dart
+++ test/i18next_test.dart
@@ -21,25 +21,33 @@ void main() {
     );
   }
 
-  group('when has multiple namespaces', () {
-    const Map<String, Object> namespace1 = {'myKey': 'My first value'},
-        namespace2 = {'myKey': 'My second value'};
-
-    test('with multiple namespaces', () {
+  group('given named namespaces', () {
+    setUp(() {
       i18next = I18Next(
         locale,
-        (namespace, loc) {
+        (namespace, _) {
           switch (namespace) {
             case 'ns1':
-              return namespace1;
+              return const {'key': 'My first value'};
             case 'ns2':
-              return namespace2;
+              return const {'key': 'My second value'};
           }
           return null;
         },
       );
-      expect(i18next.t('ns1:myKey'), 'My first value');
-      expect(i18next.t('ns2:myKey'), 'My second value');
+    });
+
+    test('given key for matching namespaces', () {
+      expect(i18next.t('ns1:key'), 'My first value');
+      expect(i18next.t('ns2:key'), 'My second value');
+    });
+
+    test('given key for unmatching namespaces', () {
+      expect(i18next.t('ns3:key'), 'ns3:key');
+    });
+
+    test('given key for partially matching namespaces', () {
+      expect(i18next.t('ns:key'), 'ns:key');
     });
   });
 
@@ -55,9 +63,10 @@ void main() {
     expect(i18next.t('ns:myKey'), 'My value');
   });
 
-  test('given key for unregistered namespace', () {
-    i18next = I18Next(locale, (namespace, loc) => null);
-    expect(i18next.t('ns:myKey'), 'ns:myKey');
+  test('given null namespace', () {
+    given(null);
+    expect(i18next.t('someKey'), 'someKey');
+    expect(i18next.t('some.key'), 'some.key');
   });
 
   test('given null key', () {
@@ -73,202 +82,50 @@ void main() {
   test('given a non-existing key', () {
     given({});
     expect(i18next.t('someKey'), 'someKey');
+    expect(i18next.t('some.key'), 'some.key');
   });
 
-  test('given a nested string key', () {
-    given({
-      'my': {
-        'key': 'This is my key',
-        'nested': {
-          'key': 'This is a more nested key',
+  group('given nested data', () {
+    test('given a matching nested key', () {
+      given({
+        'my': {
+          'key': 'This is my key',
+          'nested': {
+            'key': 'This is a more nested key',
+          }
         }
-      }
-    });
-    expect(i18next.t('my.key'), 'This is my key');
-    expect(i18next.t('my.nested.key'), 'This is a more nested key');
-  });
-
-  test('given a partially matching nested key', () {
-    given({
-      'my': {
-        'nested': {'key': 'This is a more nested key'},
-      }
-    });
-    expect(i18next.t('my'), 'my');
-    expect(i18next.t('my.nested'), 'my.nested');
-  });
-
-  group('pluralizable data', () {
-    setUp(() {
-      given(const {
-        'friend': 'A friend',
-        'friend_plural': '{{count}} friends',
       });
+      expect(i18next.t('my.key'), 'This is my key');
+      expect(i18next.t('my.nested.key'), 'This is a more nested key');
     });
 
-    test('without count', () {
-      expect(i18next.t('friend'), 'A friend');
-    });
-
-    test('given count', () {
-      expect(i18next.t('friend', count: 0), '0 friends');
-      expect(i18next.t('friend', count: 1), 'A friend');
-      expect(i18next.t('friend', count: -1), '-1 friends');
-      expect(i18next.t('friend', count: 99), '99 friends');
-    });
-
-    test('given count and unmmaped context', () {
-      expect(
-        i18next.t('friend', count: 1, context: 'something'),
-        'A friend',
-      );
-      expect(
-        i18next.t('friend', count: 99, context: 'something'),
-        '99 friends',
-      );
-    });
-
-    // TODO: add special pluralization rules
-  });
-
-  group('contextual data', () {
-    setUp(() {
-      given(const {
-        'friend': 'A friend',
-        'friend_male': 'A boyfriend',
-        'friend_female': 'A girlfriend',
+    test('given a partially matching nested key', () {
+      given({
+        'my': {
+          'nested': {'key': 'This is a more nested key'},
+        }
       });
+      expect(i18next.t('my'), 'my');
+      expect(i18next.t('my.nested'), 'my.nested');
     });
 
-    test('without context', () {
-      expect(i18next.t('friend'), 'A friend');
-    });
-
-    test('given mapped context', () {
-      expect(i18next.t('friend', context: 'male'), 'A boyfriend');
-      expect(i18next.t('friend', context: 'female'), 'A girlfriend');
-    });
-
-    test('given unmaped context', () {
-      expect(i18next.t('friend', context: 'other'), 'A friend');
-    });
-
-    test('given mapped context and count', () {
-      expect(
-        i18next.t('friend', context: 'male', count: 0),
-        'A boyfriend',
-      );
-      expect(
-        i18next.t('friend', context: 'male', count: 1),
-        'A boyfriend',
-      );
-    });
-
-    test('given unmapped context and count', () {
-      expect(
-        i18next.t('friend', context: 'other', count: 1),
-        'A friend',
-      );
-      expect(
-        i18next.t('friend', context: 'other', count: 99),
-        'A friend',
-      );
-    });
-  });
-
-  group('contextual and pluralized data', () {
-    setUp(() {
-      given(const {
-        'friend': 'A friend',
-        'friend_plural': '{{count}} friends',
-        'friend_male': 'A boyfriend',
-        'friend_male_plural': '{{count}} boyfriends',
-        'friend_female': 'A girlfriend',
-        'friend_female_plural': '{{count}} girlfriends',
+    test('given a over matching nested key', () {
+      given({
+        'my': {
+          'nested': {'key': 'This is a more nested key'},
+        }
       });
-    });
-
-    test('given mapped context and count', () {
-      expect(
-        i18next.t('friend', context: 'male', count: 0),
-        '0 boyfriends',
-      );
-      expect(
-        i18next.t('friend', context: 'male', count: 1),
-        'A boyfriend',
-      );
-      expect(
-        i18next.t('friend', context: 'female', count: 0),
-        '0 girlfriends',
-      );
-      expect(
-        i18next.t('friend', context: 'female', count: 1),
-        'A girlfriend',
-      );
-    });
-
-    test('given unmmaped context and count', () {
-      expect(
-        i18next.t('friend', context: 'other', count: 0),
-        '0 friends',
-      );
-      expect(
-        i18next.t('friend', context: 'other', count: 1),
-        'A friend',
-      );
+      expect(i18next.t('my.nested.key.here'), 'my.nested.key.here');
     });
   });
 
-  group('interpolation', () {
-    setUp(() {
-      given(const {'myKey': '{{first}}, {{second}}, and then {{third}}!'});
-    });
-
-    test('given empty interpolation', () {
-      given({'key': 'This is some {{}}'});
-      expect(i18next.t('key'), 'This is some {{}}');
-    });
-
-    test('given non matching arguments', () {
-      expect(
-        i18next.t('myKey', variables: {'none': 'none'}),
-        '{{first}}, {{second}}, and then {{third}}!',
-      );
-    });
-
-    test('given partially matching arguments', () {
-      expect(
-        i18next.t('myKey', variables: {'first': 'fst'}),
-        'fst, {{second}}, and then {{third}}!',
-      );
-      expect(
-        i18next.t('myKey', variables: {'first': 'fst', 'third': 'trd'}),
-        'fst, {{second}}, and then trd!',
-      );
-    });
-
-    test('given all matching arguments', () {
-      expect(
-        i18next.t('myKey', variables: {
-          'first': 'fst',
-          'second': 'snd',
-          'third': 'trd',
-        }),
-        'fst, snd, and then trd!',
-      );
-    });
-
-    test('given extra matching arguments', () {
-      expect(
-        i18next.t('myKey', variables: {
-          'first': 'fst',
-          'second': 'snd',
-          'third': 'trd',
-          'none': 'none',
-        }),
-        'fst, snd, and then trd!',
-      );
-    });
+  test('given overriding locale', () {
+    const anotherLocale = Locale('another');
+    i18next = I18Next(locale, expectAsync2((_, loc) {
+      expect(loc, anotherLocale);
+      return const {'key': 'my value'};
+    }));
+    expect(i18next.t('key', locale: anotherLocale), 'my value');
   });
 
   group('given formatter', () {
@@ -352,25 +209,226 @@ void main() {
     });
   });
 
-  test('given overriding locale', () {
-    const data = {'key': 'my value'};
-    const anotherLocale = Locale('another');
-    i18next = I18Next(locale, expectAsync2((_, loc) {
-      expect(loc, anotherLocale);
-      return data;
-    }));
-    expect(i18next.t('key', locale: anotherLocale), 'my value');
+  group('pluralization', () {
+    setUp(() {
+      given(const {
+        'friend': 'A friend',
+        'friend_plural': '{{count}} friends',
+      });
+    });
+
+    test('given key without count', () {
+      expect(i18next.t('friend'), 'A friend');
+    });
+
+    test('given key with count', () {
+      expect(i18next.t('friend', count: 0), '0 friends');
+      expect(i18next.t('friend', count: 1), 'A friend');
+      expect(i18next.t('friend', count: -1), '-1 friends');
+      expect(i18next.t('friend', count: 99), '99 friends');
+    });
+
+    test('given key with count in variables', () {
+      expect(i18next.t('friend', variables: {'count': 0}), '0 friends');
+      expect(i18next.t('friend', variables: {'count': 1}), 'A friend');
+      expect(i18next.t('friend', variables: {'count': -1}), '-1 friends');
+      expect(i18next.t('friend', variables: {'count': 99}), '99 friends');
+    });
+
+    test('given key with both count property and in variables', () {
+      expect(
+        i18next.t('friend', count: 0, variables: {'count': 1}),
+        'A friend',
+      );
+      expect(
+        i18next.t('friend', count: 1, variables: {'count': 0}),
+        '0 friends',
+      );
+    });
+
+    test('given key with count and unmmaped context', () {
+      expect(
+        i18next.t('friend', count: 1, context: 'something'),
+        'A friend',
+      );
+      expect(
+        i18next.t('friend', count: 99, context: 'something'),
+        '99 friends',
+      );
+    });
+
+    // TODO: add special pluralization rules
+  });
+
+  group('contextualization', () {
+    setUp(() {
+      given(const {
+        'friend': 'A friend',
+        'friend_male': 'A boyfriend',
+        'friend_female': 'A girlfriend',
+      });
+    });
+
+    test('given key without context', () {
+      expect(i18next.t('friend'), 'A friend');
+    });
+
+    test('given key with mapped context', () {
+      expect(i18next.t('friend', context: 'male'), 'A boyfriend');
+      expect(i18next.t('friend', context: 'female'), 'A girlfriend');
+    });
+
+    test('given key with mapped context in variables', () {
+      expect(
+        i18next.t('friend', variables: {'context': 'male'}),
+        'A boyfriend',
+      );
+      expect(
+        i18next.t('friend', variables: {'context': 'female'}),
+        'A girlfriend',
+      );
+    });
+
+    test('given key with both mapped context property and in variables', () {
+      expect(
+        i18next.t('friend', context: 'female', variables: {'context': 'male'}),
+        'A boyfriend',
+      );
+      expect(
+        i18next.t('friend', context: 'male', variables: {'context': 'female'}),
+        'A girlfriend',
+      );
+    });
+
+    test('given key with unmaped context', () {
+      expect(i18next.t('friend', context: 'other'), 'A friend');
+    });
+
+    test('given key with mapped context and count', () {
+      expect(
+        i18next.t('friend', context: 'male', count: 0),
+        'A boyfriend',
+      );
+      expect(
+        i18next.t('friend', context: 'male', count: 1),
+        'A boyfriend',
+      );
+    });
+
+    test('given key with unmapped context and count', () {
+      expect(
+        i18next.t('friend', context: 'other', count: 1),
+        'A friend',
+      );
+      expect(
+        i18next.t('friend', context: 'other', count: 99),
+        'A friend',
+      );
+    });
+  });
+
+  group('contextualization and pluralization', () {
+    setUp(() {
+      given(const {
+        'friend': 'A friend',
+        'friend_plural': '{{count}} friends',
+        'friend_male': 'A boyfriend',
+        'friend_male_plural': '{{count}} boyfriends',
+        'friend_female': 'A girlfriend',
+        'friend_female_plural': '{{count}} girlfriends',
+      });
+    });
+
+    test('given key with mapped context and count', () {
+      expect(
+        i18next.t('friend', context: 'male', count: 0),
+        '0 boyfriends',
+      );
+      expect(
+        i18next.t('friend', context: 'male', count: 1),
+        'A boyfriend',
+      );
+      expect(
+        i18next.t('friend', context: 'female', count: 0),
+        '0 girlfriends',
+      );
+      expect(
+        i18next.t('friend', context: 'female', count: 1),
+        'A girlfriend',
+      );
+    });
+
+    test('given key with unmmaped context and count', () {
+      expect(
+        i18next.t('friend', context: 'other', count: 0),
+        '0 friends',
+      );
+      expect(
+        i18next.t('friend', context: 'other', count: 1),
+        'A friend',
+      );
+    });
+  });
+
+  group('interpolation', () {
+    setUp(() {
+      given(const {'myKey': '{{first}}, {{second}}, and then {{third}}!'});
+    });
+
+    test('given empty interpolation', () {
+      given({'key': 'This is some {{}}'});
+      expect(i18next.t('key'), 'This is some {{}}');
+    });
+
+    test('given non matching arguments', () {
+      expect(
+        i18next.t('myKey', variables: {'none': 'none'}),
+        '{{first}}, {{second}}, and then {{third}}!',
+      );
+    });
+
+    test('given partially matching arguments', () {
+      expect(
+        i18next.t('myKey', variables: {'first': 'fst'}),
+        'fst, {{second}}, and then {{third}}!',
+      );
+      expect(
+        i18next.t('myKey', variables: {'first': 'fst', 'third': 'trd'}),
+        'fst, {{second}}, and then trd!',
+      );
+    });
+
+    test('given all matching arguments', () {
+      expect(
+        i18next.t('myKey', variables: {
+          'first': 'fst',
+          'second': 'snd',
+          'third': 'trd',
+        }),
+        'fst, snd, and then trd!',
+      );
+    });
+
+    test('given extra matching arguments', () {
+      expect(
+        i18next.t('myKey', variables: {
+          'first': 'fst',
+          'second': 'snd',
+          'third': 'trd',
+          'none': 'none',
+        }),
+        'fst, snd, and then trd!',
+      );
+    });
   });
 
   group('nesting', () {
     test('when nested key is not found', () {
-      given({
-        'key': r'This is my $t(anotherKey)',
-      });
+      given({'key': r'This is my $t(anotherKey)'});
       expect(i18next.t('key'), r'This is my $t(anotherKey)');
     });
 
-    test('simple key substitutions', () {
+    test('given multiple simple key substitutions', () {
       given({
         'nesting1': r'1 $t(nesting2)',
         'nesting2': r'2 $t(nesting3)',
@@ -390,7 +448,7 @@ void main() {
       );
     });
 
-    test('interpolation of nested variables', () {
+    test('nested interpolations', () {
       given({
         'key1': 'hello {{name}}',
         'key2': r'say: $t(key1)',
@@ -401,7 +459,7 @@ void main() {
       );
     });
 
-    test('with pluralization and interpolation ', () {
+    test('nested pluralization and interpolation ', () {
       given({
         'girlsAndBoys': r'$t(girls, {"count": {{girls}} }) and {{count}} boy',
         'girlsAndBoys_plural':
